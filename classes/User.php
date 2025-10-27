a18d0942acf1e7b3403d23000340f7653c205461
Sherilynです <aicsssanchez@gmail.com>
2025-10-28 02:42:29 +0800

    Sanchez - User.php PATIENT

---
+++ classes/User.php
@@ -0,0 +1,36 @@
+<?php
+class User {
+    private $conn;
+    private $table = "USERS";
+
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+    public function emailExists($email) {
+        $sql = "SELECT USER_ID FROM {$this->table} WHERE USER_NAME = ? LIMIT 1";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([$email]);
+        return $stmt->rowCount() > 0;
+    }
+
+    public function create($data) {
+        $sql = "INSERT INTO {$this->table} 
+                (USER_NAME, PASSWORD, PAT_ID, USER_CREATED_AT, USER_UPDATED_AT, USER_IS_SUPERADMIN)
+                VALUES (:user_name, :password, :pat_id, NOW(), NOW(), FALSE)";
+
+        $stmt = $this->conn->prepare($sql);
+        return $stmt->execute([
+            ':user_name' => $data['user_name'],
+            ':password'  => $data['password'],
+            ':pat_id'    => $data['pat_id']
+        ]);
+    }
+
+    public function findByUsername($username) {
+        $sql = "SELECT * FROM {$this->table} WHERE USER_NAME = ? LIMIT 1";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([$username]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+}
