29e71693a8c85e399d94dd518be2f5b528050349
Sherilynです <aicsssanchez@gmail.com>
2025-11-29 03:45:25 +0800

    railway

--- config/Database.php
+++ config/Database.php
@@ -1,26 +1,34 @@
 <?php
-// config/Database.php
-     class Database { 
-          private $host = "localhost";
-          private $dbname = "medical_booking";
-          private $username = "root";
-          private $password = "";
-          private $conn;
 
-          public function connect() {
-               if ($this->conn == null) {
-                    try {
-                         $this->conn = new PDO("mysql:host={$this->host};dbname={$this->dbname}",
-                                        $this->username, $this->password
-                         );
-                         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                    }catch(PDOException $e) {
-                         echo "Connected failed: " . $e->getMessage();
-                    }
-               }
+class Database {
+    private $host;
+    private $db_name;
+    private $username;
+    private $password;
+    private $port;
+    public $conn;
 
-               return $this->conn;
-          }
-     }
- ?>
+    public function __construct() {
+        $this->host = getenv('MYSQLHOST');
+        $this->db_name = getenv('MYSQLDATABASE');
+        $this->username = getenv('MYSQLUSER');
+        $this->password = getenv('MYSQLPASSWORD');
+        $this->port = getenv('MYSQLPORT');
+    }
 
+    public function connect() {
+        $this->conn = null;
+
+        try {
+            $dsn = "mysql:host={$this->host};port={$this->port};dbname={$this->db_name};charset=utf8mb4";
+            $this->conn = new PDO($dsn, $this->username, $this->password);
+
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        } catch (PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
+
+        return $this->conn;
+    }
+}
